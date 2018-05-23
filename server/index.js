c87138b943939835ee05d638acfc0b8025fab983
jamesvertica <35894238+jamesvertica@users.noreply.github.com>
2018-05-23 11:50:29 -0500

    Added templated Axios requests & new dependencies

--- server/index.js
+++ server/index.js
@@ -3,21 +3,31 @@ let app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-app.post('/repos', function (req, res) {
-  // TODO - your code here!
-  // This route should take the github username provided
-  // and get the repo information from the github API, then
-  // save the repo information in the database
+app.post('/repos', function(req, res) {
+    // TODO - your code here!
+    // This route should take the github username provided
+    // and get the repo information from the github API, then
+    // save the repo information in the database
+
+    // axios.post('/save', { firstName: 'Marlon', lastName: 'Bernardes' })
+    // .then(function(response){
+    //   console.log('saved successfully')
+    // });  
 });
 
-app.get('/repos', function (req, res) {
-  // TODO - your code here!
-  // This route should send back the top 25 repos
+app.get('/repos', function(req, res) {
+    // TODO - your code here!
+    // This route should send back the top 25 repos
+
+    // axios.get('https://api.github.com/users/' + username)
+    // .then(function(response){
+    //   console.log(response.data); // ex.: { user: 'Your User'}
+    //   console.log(response.status); // ex.: 200
+    // });
 });
 
 let port = 1128;
 
 app.listen(port, function() {
-  console.log(`listening on port ${port}`);
+    console.log(`listening on port ${port}`);
 });
-
