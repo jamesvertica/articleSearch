bf89cede5c5c38ace63f1a4b19f7375eb4c90659
jamesvertica <35894238+jamesvertica@users.noreply.github.com>
2018-05-23 12:20:54 -0500

    building schema

--- database/index.js
+++ database/index.js
@@ -2,15 +2,19 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+    // TODO: your schema here!
+    user: '',
+    topRepo: '',
+    //Potential Reqs
+    //Many users to many repos
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+let save = ( /* TODO */ ) => {
+    // TODO: Your code here
+    // This function should save a repo or repos to
+    // the MongoDB
 }
 
 module.exports.save = save;
