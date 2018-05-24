503d125a67bbacfb54c07227f481748877afb42b
jamesvertica <35894238+jamesvertica@users.noreply.github.com>
2018-05-23 19:07:35 -0500

    Finished Schema, getRepoByUsername and 'this' context of Search.jsx

--- helpers/github.js
+++ helpers/github.js
@@ -1,20 +1,20 @@
 const request = require('request');
 const config = require('../config.js');
+const axios = require('axios');
 
-let getReposByUsername = () => {
+let getReposByUsername = (username) => {
     // TODO - Use the request module to request repos for a specific
     // user from the github API
-    // req.body.name
     // The options object has been provided to help you out, 
     // but you'll have to fill in the URL
-    let options = {
-        url: 'FILL ME IN',
+
+
+    axios.get('https://api.github.com/users/${username}/repos', {
         headers: {
             'User-Agent': 'request',
             'Authorization': `token ${config.TOKEN}`
         }
-    };
-
+    })
 }
 
 module.exports.getReposByUsername = getReposByUsername;
