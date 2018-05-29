49b644d19994ad0fd125a17d828f44ca53d4ece9
jamesvertica <35894238+jamesvertica@users.noreply.github.com>
2018-05-29 08:42:27 -0500

    added paper style

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper'
 
 class Search extends React.Component {
   constructor(props) {
@@ -22,9 +28,9 @@ class Search extends React.Component {
 
   render() {
     return (<div>
-      <h4>Add more articles!</h4>
+      <Typography variant="display"> Read Below: </Typography>
       Enter a topic: <TextField value={this.state.topic} onChange={this.onChange.bind(this)}/>       
-      <Button variant="raised" color="primary" onClick={this.search.bind(this)}> Archive Articles </Button>
+      <Button variant="raised" color="primary" onClick={this.search.bind(this)}> Lookup </Button>
     </div>) 
   }
 }
