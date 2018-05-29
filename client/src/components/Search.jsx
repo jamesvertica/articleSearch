c236b14bb4c3f8af8727816866d1f9e09dc34774
jamesvertica <35894238+jamesvertica@users.noreply.github.com>
2018-05-29 18:56:40 -0500

    alternate styling

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper'
 import TextField from '@material-ui/core/TextField';
 
 class Search extends React.Component {
--- client/src/components/listItem.jsx
+++ client/src/components/listItem.jsx
@@ -4,7 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
+import CardMedia from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper'
@@ -14,13 +14,15 @@ const ArticleItem = (props) => {
     console.log(props.article, 'AH')
 
     return (
-        <Card>
+        <Paper>
             <CardHeader>{props.article.headline} </CardHeader>
-            <CardContent>{props.article.snippet}</CardContent>
-            <CardContent>{props.article.published}</CardContent>
-            <CardContent>{props.article.wordCount}</CardContent>
-            <CardContent>{props.article.uri}</CardContent>
-        </Card>
+            <Typography variant="title">{props.article.snippet}</Typography>
+            <Typography variant="caption">{props.article.published}</Typography>
+            <CardActions>
+           <Button size="small" >{props.article.wordCount}</Button>
+                <Button size="small">{props.article.uri}></Button>
+            </CardActions>
+        </Paper>
     )
 }
 export default ArticleItem;
