fffc2707b993a48de9d61470823290dcb9b01027
jamesvertica <35894238+jamesvertica@users.noreply.github.com>
2018-05-28 23:17:00 -0500

    change env variable for port

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.get('/articles', function(req, res) {
     db.twentyFive(cb);
 });
 
-let port = process.env.port || 3000;
+let port = process.env.PORT || 3000;
 
 app.listen(port, function() {
     console.log(`listening on port ${port}`);
