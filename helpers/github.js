da28acfc9f78aaea0141fa10a186b5158c9afe30
jamesvertica <35894238+jamesvertica@users.noreply.github.com>
2018-05-28 23:14:18 -0500

    heroku fix

--- helpers/github.js
+++ helpers/github.js
@@ -1,23 +1,22 @@
 const request = require('request');
-const config = require('../config.js');
 const save = require('../database/index.js')
 const axios = require('axios');
 
 let getArticlesByTopic = (topic, callback) => {
-request({
-  method: 'GET',
-  url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
-  qs: {
-    'api-key': "da39c15d7cfe4590bd649545b367b09f",
-    'q': topic,
-    'sort': "newest",
-    'hl': "true"
-  },
-}, function(err, response, body) {
-  body = JSON.parse(body);
-  callback(body.response)
-//   console.log(body.response);
-})
+    request({
+        method: 'GET',
+        url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
+        qs: {
+            'api-key': "da39c15d7cfe4590bd649545b367b09f",
+            'q': topic,
+            'sort': "newest",
+            'hl': "true"
+        },
+    }, function(err, response, body) {
+        body = JSON.parse(body);
+        callback(body.response)
+            //   console.log(body.response);
+    })
 }
 
 module.exports.getArticlesByTopic = getArticlesByTopic;
