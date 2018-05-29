212c464851184e5a41add65a2b73374940bf104e
jamesvertica <35894238+jamesvertica@users.noreply.github.com>
2018-05-29 08:33:54 -0500

    added paper style

--- client/src/components/listItem.jsx
+++ client/src/components/listItem.jsx
@@ -3,6 +3,7 @@ import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper'
